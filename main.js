58e03b4025343dff0fb93d2ea5f856e310651cbf
Adam Weiner <adamwineguy@gmail.com>
2014-09-22 14:40:00 -0600

    start of loop practice 2

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).on('ready', function() {
+  
+var animals  = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+// for(var i=0; i<animals.length; i++) {
+//  console.log(animals[i]);
+// }
+
+// for(var i=0; i<animals.length-1; i++) {
+//  console.log(animals[i]);
+// }
+
+for(var i=0; i<animals.length; i++) {
+ console.log(animals[i]);
+}
+
+});
