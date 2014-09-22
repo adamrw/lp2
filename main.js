a2d7ddc483d0a549b961dff3102d7f8e5128c6ac
Adam Weiner <adamwineguy@gmail.com>
2014-09-22 15:36:07 -0600

    finished loop practice 2

--- main.js
+++ main.js
@@ -10,8 +10,18 @@ var animals  = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
 //  console.log(animals[i]);
 // }
 
-for(var i=0; i<animals.length; i++) {
- console.log(animals[i]);
-}
+// for(var i=0; i<animals.length; i=i+2) {
+//  console.log(animals[i]);
+// }
 
+// for(var i=animals.length-1; i>-1; i--) {
+//  console.log(animals[i]);
+// }
+
+	for(var i=0; i<animals.length; i++){
+		console.log(animals[i]);
+		if(i>0 && i<animals.length-1) {
+			console.log(animals[i]);
+		}
+	}
 });
